ae9e66ac6b856f1611c3499dab8599cecb480d26
Saidi Reddy Basireddy <saidi.basireddy@bupa.com.au>
2018-05-03 12:05:56 +1000

    Logo fix

--- packages/react-scripts/template/src/App.tsx
+++ packages/react-scripts/template/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as styles from  './App.less';
 
-import logo from './logo.svg';
+const logo = require('./logo.svg');
 
 class App extends React.Component<{}, {}> {
   render() {
